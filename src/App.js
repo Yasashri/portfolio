573f13708a8794d578cffa5544592acb0526e1c8
Yasashri <ymedagedara@gmail.com>
2023-06-26 09:26:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First vercel by Yasashri
         </a>
       </header>
     </div>
