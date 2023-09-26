4de84513203f77831676e63fc5f6fb7589828b7c
Yasashri <ymedagedara@gmail.com>
2023-09-26 13:54:07 +0530

    profile pic

--- src/Components/landingPage.jsx
+++ src/Components/landingPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function landingPage() {
   return (
-    <div>landingPage</div>
-  )
+    <div className='ym-landing-page-container'>
+      <div className='ym-landing-page-top'>
+        <div className='ym-profile-image-section'>
+          <div class='profile-frame'>
+            <img
+              src='profile.jpg'
+              alt='Profile Image'
+              class='profile-image'
+            ></img>
+          </div>
+        </div>
+        <div className="ym-my-info-simple-section"></div>
+      </div>
+    </div>
+  );
 }
 
-export default landingPage
+export default landingPage;
