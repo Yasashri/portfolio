dfc117f33d19e27d0e12a39d323ec2f012682f8c
Yasashri Medagedara <ymedagedara@gmail.com>
2025-03-13 15:43:40 +0800

    Added social and other links to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import ParticleBackground from "../components/ParticleBackground";
 import Contact from "../components/Contact";
 import Resume from "../components/Resume";
 import Portfolio from "../components/Portfolio";
+import { Facebook, Github, Linkedin, Globe } from "lucide-react";
 
 const titles = [
   "I am Yasashri Medagedara",
@@ -14,11 +15,16 @@ const titles = [
 const Home = () => {
   const [currentTitle, setCurrentTitle] = useState(0);
   const [fade, setFade] = useState(true);
-  const [visibleComponent, setVisibleComponent] = useState<"contact" | "resume" | "portfolio" | null>(null);
+  const [visibleComponent, setVisibleComponent] = useState<
+    "contact" | "resume" | "portfolio" | null
+  >(null);
 
-  const toggleComponent = useCallback((component: "contact" | "resume" | "portfolio") => {
-    setVisibleComponent((prev) => (prev === component ? null : component));
-  }, []);
+  const toggleComponent = useCallback(
+    (component: "contact" | "resume" | "portfolio") => {
+      setVisibleComponent((prev) => (prev === component ? null : component));
+    },
+    []
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -35,32 +41,83 @@ const Home = () => {
   return (
     <div style={{ position: "relative", height: "100vh" }}>
       <ParticleBackground />
-      <div className="ym-container">
-        <Contact visibility={visibleComponent === "contact"} closeButton={() => setVisibleComponent(null)} />
-        <Resume visibility={visibleComponent === "resume"} closeButton={() => setVisibleComponent(null)} />
-        <Portfolio visibility={visibleComponent === "portfolio"} closeButton={() => setVisibleComponent(null)} />
+      <div className='ym-container'>
+        <Contact
+          visibility={visibleComponent === "contact"}
+          closeButton={() => setVisibleComponent(null)}
+        />
+        <Resume
+          visibility={visibleComponent === "resume"}
+          closeButton={() => setVisibleComponent(null)}
+        />
+        <Portfolio
+          visibility={visibleComponent === "portfolio"}
+          closeButton={() => setVisibleComponent(null)}
+        />
         <div className={`ym-container__name ${fade ? "fade-in" : "fade-out"}`}>
           {titles[currentTitle]}
         </div>
 
-        <div className="ym-container__menu">
+        <div className='ym-container__menu'>
           <div
-            className="ym-container__menu-item ym-container__menu-item--portfolio"
+            className='ym-container__menu-item ym-container__menu-item--portfolio'
             onClick={() => toggleComponent("portfolio")}
           >
             Portfolio
           </div>
           <div
-            className="ym-container__menu-item ym-container__menu-item--resume"
+            className='ym-container__menu-item ym-container__menu-item--resume'
             onClick={() => toggleComponent("resume")}
           >
             Resume
           </div>
           <div
-            className="ym-container__menu-item ym-container__menu-item--contact"
+            className='ym-container__menu-item ym-container__menu-item--contact'
             onClick={() => toggleComponent("contact")}
           >
             Contact
+          </div>
+        </div>
+        <div className='yms-link-set'>
+          <div className='yms-link-set--data'>
+            <Github size={20} />
+            <a
+              href='https://github.com/Yasashri'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              GitHub
+            </a>
+          </div>
+          <div className='yms-link-set--data'>
+            <Globe size={20} />
+            <a
+              href='https://medium.com/@ymedagedara'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Medium
+            </a>
+          </div>
+          <div className='yms-link-set--data'>
+            <Facebook size={20} />
+            <a
+              href='https://www.facebook.com/yasashri/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Facebook
+            </a>
+          </div>
+          <div className='yms-link-set--data'>
+            <Linkedin size={20} />
+            <a
+              href='https://www.linkedin.com/in/yasashri/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              LinkedIn
+            </a>
           </div>
         </div>
       </div>
