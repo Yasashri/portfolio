5346d1d7ddccb9b5886e3fca4bbb51c83fe03c76
Yasashri Medagedara <ymedagedara@gmail.com>
2025-03-15 10:08:47 +0800

    Added a spinner loader

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,6 +18,7 @@ const Home = () => {
   const [visibleComponent, setVisibleComponent] = useState<
     "contact" | "resume" | "portfolio" | null
   >(null);
+  const [loading, setLoading] = useState(true);
 
   const toggleComponent = useCallback(
     (component: "contact" | "resume" | "portfolio") => {
@@ -25,6 +26,11 @@ const Home = () => {
     },
     []
   );
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -40,87 +46,98 @@ const Home = () => {
 
   return (
     <div style={{ position: "relative", height: "100vh" }}>
-      <ParticleBackground />
-      <div className='ym-container'>
-        <Contact
-          visibility={visibleComponent === "contact"}
-          closeButton={() => setVisibleComponent(null)}
-        />
-        <Resume
-          visibility={visibleComponent === "resume"}
-          closeButton={() => setVisibleComponent(null)}
-        />
-        <Portfolio
-          visibility={visibleComponent === "portfolio"}
-          closeButton={() => setVisibleComponent(null)}
-        />
-        <div className={`ym-container__name ${fade ? "fade-in" : "fade-out"}`}>
-          {titles[currentTitle]}
+      {loading ? (
+        <div className='loading-screen'>
+          <div className='loading-spinner'></div>
+          <p style={{ marginLeft: 10 }}>Welcome..</p>
         </div>
+      ) : (
+        <>
+          <ParticleBackground />
+          <div className='ym-container'>
+            <Contact
+              visibility={visibleComponent === "contact"}
+              closeButton={() => setVisibleComponent(null)}
+            />
+            <Resume
+              visibility={visibleComponent === "resume"}
+              closeButton={() => setVisibleComponent(null)}
+            />
+            <Portfolio
+              visibility={visibleComponent === "portfolio"}
+              closeButton={() => setVisibleComponent(null)}
+            />
+            <div
+              className={`ym-container__name ${fade ? "fade-in" : "fade-out"}`}
+            >
+              {titles[currentTitle]}
+            </div>
 
-        <div className='ym-container__menu'>
-          <div
-            className='ym-container__menu-item ym-container__menu-item--portfolio'
-            onClick={() => toggleComponent("portfolio")}
-          >
-            Portfolio
+            <div className='ym-container__menu'>
+              <div
+                className='ym-container__menu-item ym-container__menu-item--portfolio'
+                onClick={() => toggleComponent("portfolio")}
+              >
+                Portfolio
+              </div>
+              <div
+                className='ym-container__menu-item ym-container__menu-item--resume'
+                onClick={() => toggleComponent("resume")}
+              >
+                Resume
+              </div>
+              <div
+                className='ym-container__menu-item ym-container__menu-item--contact'
+                onClick={() => toggleComponent("contact")}
+              >
+                Contact
+              </div>
+            </div>
+            <div className='yms-link-set'>
+              <div className='yms-link-set--data'>
+                <Github size={20} />
+                <a
+                  href='https://github.com/Yasashri'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  GitHub
+                </a>
+              </div>
+              <div className='yms-link-set--data'>
+                <Globe size={20} />
+                <a
+                  href='https://medium.com/@ymedagedara'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Medium
+                </a>
+              </div>
+              <div className='yms-link-set--data'>
+                <Facebook size={20} />
+                <a
+                  href='https://www.facebook.com/yasashri/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Facebook
+                </a>
+              </div>
+              <div className='yms-link-set--data'>
+                <Linkedin size={20} />
+                <a
+                  href='https://www.linkedin.com/in/yasashri/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
           </div>
-          <div
-            className='ym-container__menu-item ym-container__menu-item--resume'
-            onClick={() => toggleComponent("resume")}
-          >
-            Resume
-          </div>
-          <div
-            className='ym-container__menu-item ym-container__menu-item--contact'
-            onClick={() => toggleComponent("contact")}
-          >
-            Contact
-          </div>
-        </div>
-        <div className='yms-link-set'>
-          <div className='yms-link-set--data'>
-            <Github size={20} />
-            <a
-              href='https://github.com/Yasashri'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              GitHub
-            </a>
-          </div>
-          <div className='yms-link-set--data'>
-            <Globe size={20} />
-            <a
-              href='https://medium.com/@ymedagedara'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Medium
-            </a>
-          </div>
-          <div className='yms-link-set--data'>
-            <Facebook size={20} />
-            <a
-              href='https://www.facebook.com/yasashri/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Facebook
-            </a>
-          </div>
-          <div className='yms-link-set--data'>
-            <Linkedin size={20} />
-            <a
-              href='https://www.linkedin.com/in/yasashri/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              LinkedIn
-            </a>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
